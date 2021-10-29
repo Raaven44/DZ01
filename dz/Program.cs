146ab1930c586a07a9e221ea625fd3d92b0ea79d
konstantin-sh03 <k.shulika03@gmail.com>
2021-10-29 08:39:15 +0300

    dz

---
+++ dz/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Drobi d1 = new Drobi(2, 3);
+            Drobi d2 = new Drobi(-2);
+            Drobi d3 = new Drobi(2, 3, 4);
+            Console.WriteLine(d1.ToDouble());
+            Console.WriteLine((d1 * d3).ToDouble());
+            Console.WriteLine((d1 * d2).Znak);
+            Console.ReadKey();
+
+        }
+    }
+}
